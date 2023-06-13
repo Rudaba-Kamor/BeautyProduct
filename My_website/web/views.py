aabad0220629b5781001bc2424ab570ea4a7f642
Rudaba-Kamor <rudabaadninkamor@gmail.com>
2023-06-13 22:02:58 +0600

    beauty product

--- My_website/web/views.py
+++ My_website/web/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 def home(request):
   #  return HttpResponse("Hello world!")
   return render(request,'web/home.html')
+
+def products(request):
+  #  return HttpResponse("Hello world!")
+  return render(request,'web/products.html')
